f89b398354f0bb6e42d81a51a17e4e5ad1ac96aa
Revant Cherian Idikulay <RevantCI@users.noreply.github.com>
2021-05-12 17:55:40 +0530

    Added language select component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as ApplicationBar } from './ApplicationBar';
 export { default as BibleDropDown } from './BibleDropDown';
+export { default as LanguageSelect } from './LanguageSelect';
--- src/store/api.js
+++ src/store/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const baseURL = process.env.REACT_APP_BASE_URL || 'http://128.199.18.6/v2/';
+const baseURL = process.env.REACT_APP_BASE_URL || 'http://206.189.131.230/v2/';
 export const API = axios.create({ baseURL: baseURL, timeout: 15000 });
 export const CancelToken = axios.CancelToken;

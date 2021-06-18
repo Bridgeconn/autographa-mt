15e79cd62d469bc90bd3d5c76fc1694f0a9b9331
Jincy <jincy370@gmail.com>
2021-06-18 08:20:05 +0530

    Changes done according to the review

--- src/components/ProjectList/ProjectContext.js
+++ src/components/ProjectList/ProjectContext.js
@@ -22,7 +22,6 @@ const ProjectsContextProvider = (props) => {
     <ProjectsContext.Provider
       value={{
         projects: projects,
-        reload: reload,
         setReload: setReload,
         activeProjects: activeProjects,
         setActiveProjects: setActiveProjects,
